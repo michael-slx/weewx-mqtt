c02eda715de196b1c0a52047dea9c840b0118341
Michael Schantl <floss@schantl-lx.at>
2023-06-02 18:24:39 +0200

    Add license info

--- install.py
+++ install.py
@@ -1,3 +1,18 @@
+# Copyright 2023 Michael Schantl and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import configobj
 
 from weecfg.extension import ExtensionInstaller, ExtensionEngine
@@ -38,10 +53,15 @@ class MqttInstaller(ExtensionInstaller):
         }
         config_dict['StdRESTful']['MQTT'] = mqtt_config
 
-        config_dict['StdRESTful'].comments['MQTT'] = ["", "Configuration for uploading LOOP data to an MQTT broker"]
-        config_dict['StdRESTful']['MQTT'].comments['enable'] = ["", "Enable/disable this service"]
-        config_dict['StdRESTful']['MQTT'].comments['host'] = ["", "Hostname/IP of MQTT broker"]
-        config_dict['StdRESTful']['MQTT'].comments['user'] = ["", "Credentials"]
-        config_dict['StdRESTful']['MQTT'].comments['topic'] = ["", "Prefix for topics"]
+        config_dict['StdRESTful'].comments['MQTT'] = [
+            "", "Configuration for uploading LOOP data to an MQTT broker"]
+        config_dict['StdRESTful']['MQTT'].comments['enable'] = [
+            "", "Enable/disable this service"]
+        config_dict['StdRESTful']['MQTT'].comments['host'] = [
+            "", "Hostname/IP of MQTT broker"]
+        config_dict['StdRESTful']['MQTT'].comments['user'] = [
+            "", "Credentials"]
+        config_dict['StdRESTful']['MQTT'].comments['topic'] = [
+            "", "Prefix for topics"]
 
         return True
